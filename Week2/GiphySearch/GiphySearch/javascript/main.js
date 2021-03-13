586c7f630423807ce32a1707569cc368702267eb
Md. Apple Mahmud <mahmud170052@diit.edu.bd>
2021-03-13 13:27:35 +0600

    Grabing Data from api

--- Week2/GiphySearch/GiphySearch/javascript/main.js
+++ Week2/GiphySearch/GiphySearch/javascript/main.js
@@ -2,19 +2,42 @@
 document.querySelector(".apple").addEventListener('click',function(){
 	var input=document.querySelector(".js-userinput").value;
 	//console.log(input);
+	show(input);
 });
+
+
 
 document.querySelector(".js-userinput").addEventListener('keyup',function(e){
 	var input=document.querySelector(".js-userinput").value;
 	if(e.which===13){
 	//console.log(input);
-	var sh=document.querySelector(".dis");
-
-	console.log(sh);
-	sh.innerHTML=input;
-
+	//var sh=document.querySelector(".dis");
+	show(input);
 	}
 });
 
+//Api 
+//ajax request
+var url="http://api.giphy.com/v1/gifs/search?api_key=VuMSAatfDGEFi1NpnLRMgCCSgoqBIabg&q=dog"
+var ajaxCall=new XMLHttpRequest();
+ajaxCall.open('GET',url);
+ajaxCall.send()
+//console.log(ajaxCall);
+ajaxCall.addEventListener('load',function(e){
+	var data=e.target.response;
+	console.log(e);
+	show(data);
+
+});
+
+
+
+
+
+//Show 
+function show(input){
+var sh=document.querySelector(".dis");
+sh.innerHTML=input;
+}
 
 //
