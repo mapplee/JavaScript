429d2b0d3cc31f1ecb632d6b88f476e3da8bc769
Md. Apple Mahmud <mahmud170052@diit.edu.bd>
2021-03-13 17:57:52 +0600

    Make query +Play with JSON.parse()

--- Week2/GiphySearch/GiphySearch/javascript/main.js
+++ Week2/GiphySearch/GiphySearch/javascript/main.js
@@ -21,22 +21,16 @@ document.querySelector(".js-userinput").addEventListener('keyup',function(e){
 function pushToApi(input){
 	
 	var search=input;
-	/*for(i=0;i<search.length;i++)
-	{
-		if(search[i]===' ')
-			search[i].replace(' ','+');
-		console.log(search[i]);
-	}*/
 	search=search.replace(' ','+');
 	console.log(search);
 
-var url="http://api.giphy.com/v1/gifs/search?api_key=VuMSAatfDGEFi1NpnLRMgCCSgoqBIabg&q="+search;
-console.log(url);
-var ajaxCall=new XMLHttpRequest();
-ajaxCall.open('GET',url);
-ajaxCall.send();
-//console.log(ajaxCall);
-ajaxCall.addEventListener('load',function(e){
+	var url="http://api.giphy.com/v1/gifs/search?api_key=VuMSAatfDGEFi1NpnLRMgCCSgoqBIabg&q="+search;
+	console.log(url);
+	var ajaxCall=new XMLHttpRequest();
+	ajaxCall.open('GET',url);
+	ajaxCall.send();
+	//console.log(ajaxCall);
+	ajaxCall.addEventListener('load',function(e){
 	var data=e.target.response;
 	//console.log(e);
 	show(data);
@@ -51,10 +45,17 @@ ajaxCall.addEventListener('load',function(e){
 //Show 
 function show(data){
 	var raw=JSON.parse(data);
-	console.log(raw);
-var sh=document.querySelector(".dis");
-var link=raw.data[0].images.fixed_height.url;
-sh.innerHTML='<img src="'+link+'">';
+	//console.log(raw);
+	//var link=raw.data[0].images.fixed_height.url;
+	var link=raw.data;
+	console.log(link);
+	var sh=document.querySelector(".dis");
+	sh.innerHTML=' ';
+	link.forEach(function(img){
+	var nlink=img.images.fixed_height.url;
+	
+	sh.innerHTML+='<img src="'+nlink+'class="container-image">';
+	});
 }
 
 //
