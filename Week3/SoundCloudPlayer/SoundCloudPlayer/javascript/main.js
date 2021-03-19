7527e320b20b259eb5163e8423f3bcd7ed0d7ec2
Md. Apple Mahmud <mahmud170052@diit.edu.bd>
2021-03-20 00:32:18 +0600

    Before Finalize
    Solve track list. Add reset button .Add list

--- Week3/SoundCloudPlayer/SoundCloudPlayer/javascript/main.js
+++ Week3/SoundCloudPlayer/SoundCloudPlayer/javascript/main.js
@@ -4,7 +4,7 @@ UserInput.withclick=function()
 {
 	document.querySelector(".btn_1").addEventListener('click',function(){
 			var  inputValue=document.querySelector(".input-search").value;
-			console.log(inputValue);
+			//console.log(inputValue);
 			var check=1;
 			SoundCloud.SerachQuery(inputValue,check);
 	});
@@ -13,13 +13,26 @@ UserInput.withclick_Track=function()
 {
 	document.querySelector(".btn_2").addEventListener('click',function(){
 			var  inputValue=document.querySelector(".input-search").value;
-			console.log(inputValue);
+			//console.log(inputValue);
 			var check=2;
 			SoundCloud.SerachQuery(inputValue,check);
 	});
 }
+UserInput.withEnterKey=function()
+{
+	document.querySelector(".input-search").addEventListener('keyup',function(e){
+		var inputValue=document.querySelector(".input-search").value;
+		if(e.which===13)
+		{
+			var check=prompt("Enter 1 for playlists or any key for Treck");
+			SoundCloud.SerachQuery(inputValue,check);
+		}
+
+	});
+}
 UserInput.withclick();
 UserInput.withclick_Track();
+UserInput.withEnterKey();
 
 var SoundCloud={};
 
@@ -63,7 +76,9 @@ SC.get('/tracks',{
 SoundCloud.CreateDiv=function(tracks)
 {
 	tracks.forEach(function(track){
-		console.log(track);
+		//console.log(track);
+			var list=document.querySelector('.list');
+			list.innerHTML="";
 			var card=document.createElement('div');
 			card.classList.add("card");
 			var searchResult=document.querySelector(".js-search-results");
@@ -115,7 +130,7 @@ SoundCloud.GetEmebed=function(track_link){
 SC.oEmbed(track_link, {
   auto_play: true
 }).then(function(embed){
-  console.log('oEmbed response: ', embed);
+ // console.log('oEmbed response: ', embed);
   var sidebar=document.querySelector('.js-playlist');
   var box=document.createElement('div');
   box.innerHTML=embed.html;
@@ -128,3 +143,31 @@ SC.oEmbed(track_link, {
 
 var sidebar=document.querySelector(".js-playlist");
 sidebar.innerHTML=localStorage.getItem("key");
+
+
+var btnList=document.querySelector(".btn_3");
+btnList.addEventListener('click',function(){
+	var reset=document.createElement('button');
+	reset.classList.add("search","icon","js-submit","btn_4");
+	reset.innerHTML="reset";
+	var list=document.querySelector('.list');
+	list.innerHTML="";
+	list.appendChild(reset);
+	var searchResult=document.querySelector(".js-search-results");
+	searchResult.innerHTML=localStorage.getItem("key");
+
+
+
+
+
+	var BtnReset=document.querySelector('.btn_4');
+	BtnReset.addEventListener('click',function(){
+	var sidebar=document.querySelector(".js-playlist");
+	localStorage.clear();
+	sidebar.innerHTML="";
+	var searchResult=document.querySelector(".js-search-results");
+	searchResult.innerHTML="";
+
+});
+
+});
